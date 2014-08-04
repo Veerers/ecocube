ecf4e5f2af936838d8a720993546a239e01c0961
iofjuupasli <iofjuupasli@gmail.com>
2014-08-04 10:56:26 +0300

    go up btn

--- src/app/bindings.js
+++ src/app/bindings.js
@@ -6,11 +6,31 @@ define(['knockout', 'jquery'], function (ko, $) {
             var $element = $(element);
             var options = ko.unwrap(valueAccessor());
             $element.magnificPopup(options);
-        },
-        update: function () {
-
         }
     };
+    ko.bindingHandlers.goUp = {
+        init: function (element) {
+            var start = 500;
+            var stop = 1000;
+            var $global = $(document);
+            var $element = $(element);
+            $element.click(function () {
+                $global.scrollTop(0);
+                return false;
+            });
+            $element.fadeOut(0);
+            $global.scroll(function (e) {
+                var scroll = $global.scrollTop()
+                if (scroll < start) {
+                    $element.fadeOut(0);
+                } else if (scroll >= start && scroll < stop) {
+                    $element.fadeTo(0, (scroll - start) / (stop - start));
+                } else if (scroll >= stop) {
+                    $element.fadeTo(0, 1);
+                }
+            })
+        }
+    }
     return {
 
     };
